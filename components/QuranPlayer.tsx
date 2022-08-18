afb0161ea8d593f6e8a8b6ced01d6a0167229a61
kmaslesa <kenan.maslesa@endava.com>
2022-08-18 16:32:55 +0200

    Quran Player

--- components/QuranPlayer.tsx
+++ components/QuranPlayer.tsx
@@ -2,54 +2,119 @@ import React from 'react';
 
 import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import SelectDropdown from 'react-native-select-dropdown';
 
-import {quranActions} from '../redux/slices/quranSlice';
 import {qariList} from '../data/data';
+import SoundPlayer from 'react-native-sound-player';
+import {State} from '../redux/store';
+import useQuranPlayer from '../hooks/useQuranPlayer';
+import {quranPlayerActions} from '../redux/slices/quranPlayerSlice';
 
 const PLAYER_ICONS_SIZE = 22;
 
 const QuranPlayer = () => {
   const dispatch = useDispatch();
+  const {isPlaying, isStoped, selectedQari, playingAyahIndex} = useSelector(
+    (state: State) => state.quranPlayer,
+  );
+  const [playAyahAudio, resetPlayingAyahAndWord] = useQuranPlayer();
+
+  const stopPlaying = () => {
+    SoundPlayer.stop();
+    dispatch(quranPlayerActions.setIsPlaying(false));
+    dispatch(quranPlayerActions.setIsStoped(true));
+    resetPlayingAyahAndWord();
+  };
+
+  const pausePlaying = () => {
+    SoundPlayer.pause();
+    dispatch(quranPlayerActions.setIsPlaying(false));
+  };
+
+  const resumePlaying = () => {
+    SoundPlayer.resume();
+    dispatch(quranPlayerActions.setIsPlaying(true));
+  };
+
+  const playNextAyah = () => {
+    dispatch(quranPlayerActions.setIsPlaying(true));
+    playAyahAudio(playingAyahIndex + 1);
+    dispatch(quranPlayerActions.setPlayingAyahIndex(playingAyahIndex + 1));
+  };
+
+  const playPreviousAyah = () => {
+    dispatch(quranPlayerActions.setIsPlaying(true));
+    playAyahAudio(playingAyahIndex - 1);
+    dispatch(quranPlayerActions.setPlayingAyahIndex(playingAyahIndex - 1));
+  };
 
   return (
     <View style={styles.playerContainer}>
-      <TouchableOpacity style={styles.playerIcon}>
-        <Ionicons name={'stop'} size={PLAYER_ICONS_SIZE} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.playerIcon}>
-        <Ionicons name={'play-skip-back'} size={PLAYER_ICONS_SIZE} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.playerIcon}>
-        <Ionicons name={'play'} size={PLAYER_ICONS_SIZE} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.playerIcon}>
-        <Ionicons name={'play-skip-forward'} size={PLAYER_ICONS_SIZE} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.playerIcon}>
-        <Ionicons name={'repeat'} size={PLAYER_ICONS_SIZE} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.playerIcon}>
-        <Ionicons name={'settings'} size={PLAYER_ICONS_SIZE} />
-      </TouchableOpacity>
-      <SelectDropdown
-        data={qariList.map(qari => qari.identifier)}
-        onSelect={(selectedItem, index) => {
-          console.log(selectedItem, index);
-          dispatch(quranActions.setSelectedQari(selectedItem));
-        }}
-        buttonTextAfterSelection={(selectedItem, index) => {
-          // text represented after item is selected
-          // if data array is an array of objects then return selectedItem.property to render after item is selected
-          return selectedItem;
-        }}
-        rowTextForSelection={(item, index) => {
-          // text represented for each item in dropdown
-          // if data array is an array of objects then return item.property to represent item in dropdown
-          return item;
-        }}
-      />
+      {isStoped ? (
+        <View>
+          <SelectDropdown
+            data={qariList.map(qari => qari.identifier)}
+            defaultValue={selectedQari}
+            onSelect={(selectedItem, index) => {
+              console.log(selectedItem, index);
+              dispatch(quranPlayerActions.setSelectedQari(selectedItem));
+            }}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // text represented after item is selected
+              // if data array is an array of objects then return selectedItem.property to render after item is selected
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // text represented for each item in dropdown
+              // if data array is an array of objects then return item.property to represent item in dropdown
+              return item;
+            }}
+          />
+        </View>
+      ) : (
+        <View style={styles.playerIconsContainer}>
+          <TouchableOpacity
+            style={styles.playerIcon}
+            onPress={() => stopPlaying()}>
+            <Ionicons name={'stop'} size={PLAYER_ICONS_SIZE} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.playerIcon}
+            onPress={() => playPreviousAyah()}>
+            <Ionicons name={'play-skip-back'} size={PLAYER_ICONS_SIZE} />
+          </TouchableOpacity>
+
+          {isPlaying ? (
+            <TouchableOpacity
+              style={styles.playerIcon}
+              onPress={() => pausePlaying()}>
+              <Ionicons name={'pause'} size={PLAYER_ICONS_SIZE} />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              style={styles.playerIcon}
+              onPress={() => resumePlaying()}>
+              <Ionicons name={'play'} size={PLAYER_ICONS_SIZE} />
+            </TouchableOpacity>
+          )}
+
+          <TouchableOpacity
+            style={styles.playerIcon}
+            onPress={() => playNextAyah()}>
+            <Ionicons name={'play-skip-forward'} size={PLAYER_ICONS_SIZE} />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.playerIcon}>
+            <Ionicons name={'repeat'} size={PLAYER_ICONS_SIZE} />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.playerIcon}>
+            <Ionicons name={'settings'} size={PLAYER_ICONS_SIZE} />
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
@@ -67,6 +132,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  playerIconsContainer: {
+    flexDirection: 'row',
   },
   playerIcon: {
     marginLeft: 10,
---
+++ hooks/useQuranPlayer.ts
@@ -0,0 +1,105 @@
+import {useState} from 'react';
+import {EmitterSubscription} from 'react-native';
+import SoundPlayer from 'react-native-sound-player'; //https://www.npmjs.com/package/react-native-sound-player
+import {useDispatch, useSelector} from 'react-redux';
+import {quranPlayerActions} from '../redux/slices/quranPlayerSlice';
+
+import {quranActions} from '../redux/slices/quranSlice';
+import {State} from '../redux/store';
+import {quranService} from '../services/quranService';
+import {Word} from '../shared/models';
+import {formatNumberForAudioUrl} from '../utils/formatAudioUrl';
+
+let isFinishedPlaying: boolean = false;
+let subscriptionSoundPlayer: EmitterSubscription;
+let playingAyahIndexTemp: number | undefined = -1;
+
+enum AudioCharType {
+  WORD = 'word',
+  END_ICON = 'end',
+}
+
+const useQuranPlayer = () => {
+  const dispatch = useDispatch();
+  const [playingWord, setPlayingWord] = useState<string | null>();
+  const {selectedQari} = useSelector((state: State) => state.quranPlayer);
+  const {currentPage} = useSelector((state: State) => state.quran);
+
+  const playAyahAudio = async (ayahIndex: number | undefined) => {
+    playingAyahIndexTemp = ayahIndex;
+    console.log(playingAyahIndexTemp);
+    resetPlayingAyahAndWord();
+    dispatch(quranPlayerActions.setPlayingAyahIndex(ayahIndex));
+    const data = quranService.getAyatDetailsByAyahIndex(ayahIndex);
+    if (data.page && data.page !== currentPage) {
+      dispatch(quranActions.setScrollToPage(data.page));
+    }
+    const suraOfAyah = data.sura;
+    const ayaNumber = data.ayaNumber;
+    const sura_ayah = formatNumberForAudioUrl(`${suraOfAyah}:${ayaNumber}`);
+    const audioUrl = `https://www.everyayah.com/data/${selectedQari}/${sura_ayah}.mp3`;
+
+    playAyah(audioUrl);
+  };
+
+  const playAyah = async (ayahUrl: string) => {
+    try {
+      if (subscriptionSoundPlayer && !isFinishedPlaying) {
+        subscriptionSoundPlayer.remove();
+      }
+      await SoundPlayer.playUrl(ayahUrl);
+      dispatch(quranPlayerActions.setIsStoped(false));
+      dispatch(quranPlayerActions.setIsPlaying(true));
+      const info = await SoundPlayer.getInfo();
+      console.log('getInfo', info);
+      isFinishedPlaying = false;
+      subscriptionSoundPlayer = SoundPlayer.addEventListener(
+        'FinishedPlaying',
+        () => {
+          dispatch(quranPlayerActions.setIsPlaying(false));
+          isFinishedPlaying = true;
+          subscriptionSoundPlayer.remove();
+          resetPlayingAyahAndWord();
+          let temp = playingAyahIndexTemp ? playingAyahIndexTemp + 1 : 0;
+          playAyahAudio(temp);
+        },
+      );
+    } catch (e) {
+      console.log('cannot play', e);
+    }
+  };
+
+  const resetPlayingAyahAndWord = () => {
+    dispatch(quranPlayerActions.setPlayingAyahIndex(0));
+    setPlayingWord('null');
+  };
+
+  const playWord = (word: Word | null) => {
+    resetPlayingAyahAndWord();
+    if (subscriptionSoundPlayer) {
+      subscriptionSoundPlayer.remove();
+    }
+    if (!word) {
+      return;
+    }
+    if (word.charType === AudioCharType.END_ICON) {
+      playAyahAudio(word.ayahIndex);
+      return;
+    }
+    const audioUrl = `https://audio.qurancdn.com/${word.audio}`;
+    setPlayingWord(word.audio);
+
+    try {
+      SoundPlayer.playUrl(audioUrl);
+      SoundPlayer.addEventListener('FinishedPlaying', () => {
+        resetPlayingAyahAndWord();
+      });
+    } catch (e) {
+      console.log('cannot play', e);
+    }
+  };
+
+  return [playAyahAudio, playWord, playingWord, resetPlayingAyahAndWord];
+};
+
+export default useQuranPlayer;
